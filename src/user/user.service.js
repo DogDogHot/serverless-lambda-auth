1650fab673a8f3793f00f5dcd25e0e2aca043b7c
hyunjin <jin@perfitt.io>
2022-10-23 19:30:11 +0900

    fix : prod env 환경설정 추가

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,23 +1,22 @@
 const Redis = require("ioredis");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { off } = require("../app");
 
 const saltRounds = 10;
 
 const redis = new Redis(process.env.REDIS_PORT, process.env.REDIS_URL);
 
-getUser = async (userId) => {
+const getUser = async (userId) => {
   return await redis.get(`user:${userId}`);
 };
 
-getUserList = async () => {
+const getUserList = async () => {
   let users = (await redis.keys(`user:*`)) || [];
   users = users.map((v) => v.split(":")[1]);
   return users;
 };
 
-createUser = async ({ userId, password, name }) => {
+const createUser = async ({ userId, password, name }) => {
   const secretPwd = await bcrypt.hash(password, saltRounds);
   await redis.set(
     `user:${userId}`,
@@ -25,11 +24,11 @@ createUser = async ({ userId, password, name }) => {
   );
 };
 
-login = async ({ userId, password, name }) => {
+const login = async ({ userId, password, name }) => {
   await redis.set(`user:${userId}`, JSON.stringify({ password, name }));
 };
 
-getJwt = async ({ userId, name }) => {
+const getJwt = async ({ userId, name }) => {
   const accessToken = jwt.sign({ userId, name }, process.env.JWT_KEY, {
     expiresIn: "1h",
   });
